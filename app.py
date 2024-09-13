97606f6547ce8a28d4b8dfdb5a66ee647bb70546
mingoyo27 <mingogonzalezguardiola@gmail.com>
2024-09-12 18:56:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,3 +20,15 @@ def alumnosGuardar():
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
+
+@app.route("/evento")
+def evento():
+    pusher_client = pusher.Pusher(
+        app_id="1714541",
+        key="cda1cc599395d699a2af",
+        secret="9e9c00fc36600060d9e2",
+        cluster="us2",
+        ssl=True
+    )
+    
+    pusher_client.trigger("conexion", "evento", {"message": "hello world"})
